06704785895d3abf382ca254c9d47d07fb440c9c
kenwi <kenneth@wilhelmsen.nu>
2015-12-12 18:44:44 +0100

    Added usage comment

--- nmap_auth_today.sh
+++ nmap_auth_today.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Uses the failed_auth_today.sh script to nmap them
+# Uses the failed_auth_today.sh script to nmap them. Forwards normal nmap arguments
+# Usage example repos/scripts-misc/faile_auth_today.sh -F -v
 
 nmap $@ `sh ~/repos/scripts-misc/failed_auth_today.sh`
 
