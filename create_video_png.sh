7b91f700b648a2683a8571aae48fc5f63435a557
kenwi <kenneth@wilhelmsen.nu>
2015-12-23 11:43:28 +0100

    -oaccopy to -oac copy

--- create_video_png.sh
+++ create_video_png.sh
@@ -3,5 +3,5 @@
 # Create a full HD video from all PNG's in current directory. First and second argument equals FPS and output name, respectively
 # Usage example for 60fps video: repos/scripts-misc/create_video_png.sh 60 output.mp4
 
-mencoder "mf://*.png" -mf fps=$1:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:vbitrate=7000 -vf scale=1280:720 -oaccopy -o $2
+mencoder "mf://*.png" -mf fps=$1:type=png -ovc lavc -lavcopts vcodec=mpeg4:mbd=2:trell:vbitrate=7000 -vf scale=1280:720 -oac copy -o $2
 
