a4954b16e26b36502eca32b1970de7558b89875b
root <root@scw-e53b7f>
2016-01-09 14:50:28 +0100

    Skip leading zero in day

--- failed_auth_today.sh
+++ failed_auth_today.sh
@@ -2,7 +2,7 @@
 
 # Pull todays unique ip-addresses from failed ssh attempts in auth.log 
 
-TODAY=`LANG=en_US date +"%b %d"`
+TODAY=`LANG=en_US date +"%b  %-d"`
 LOGFAIL=`cat /var/log/auth.log | grep -i "$TODAY" | grep -i failed`
 IP=`echo $LOGFAIL | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort -u`
 echo $IP
