87a0eb614a34f29128b768065f8fcd4534c10a54
kenwi <kenneth@wilhelmsen.nu>
2016-01-27 22:38:45 +0100

    Change from `` to $()

--- nmap_auth_today.sh
+++ nmap_auth_today.sh
@@ -3,5 +3,5 @@
 # Uses the failed_auth_today.sh script to nmap them. Forwards normal nmap arguments
 # Usage example repos/scripts-misc/faile_auth_today.sh -F -v
 
-nmap $@ `sh ~/repos/scripts-misc/failed_auth_today.sh`
+nmap $@ $(sh ~/repos/scripts-misc/failed_auth_today.sh)
 
