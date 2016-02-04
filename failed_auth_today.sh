5100df054155c9264e9aa5f09169b88a5aa3f967
kenwi <kenneth@wilhelmsen.nu>
2016-02-04 14:48:34 +0100

    Update date format

--- failed_auth_today.sh
+++ failed_auth_today.sh
@@ -2,7 +2,7 @@
 
 # Pull todays unique ip-addresses from failed ssh attempts in auth.log 
 
-TODAY=$(LANG=en_US date '+^%b %e')
-LOGFAIL=$(cat /var/log/auth.log | grep -i "$TODAY" | grep -i failed)
+LOGFAIL=$(grep -E "$(LANG=en_US date '+^%b %e')"'.*Failed' /var/log/auth.log)
 IP=$(echo $LOGFAIL | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sort -u)
 echo $IP
+ 
