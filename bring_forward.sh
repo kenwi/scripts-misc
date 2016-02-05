d6d9230c2419f8477f7f288b289a9603cd2915ce
kenwi <kenneth@wilhelmsen.nu>
2016-02-05 10:35:47 +0100

    Change from `` to $()

--- bring_forward.sh
+++ bring_forward.sh
@@ -6,7 +6,7 @@
 wmctrl -k on
 wmctrl -l | while read window; do
   if [[ "$window" == *$@* ]]; then
-    wmctrl -i -a `echo "$window" | cut -d " " -f 1`
+    wmctrl -i -a $(echo "$window" | cut -d " " -f 1)
   fi
 done
 	  
