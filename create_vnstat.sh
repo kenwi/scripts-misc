b209247c868182b5ba4afaf4890f388a1a1d990c
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2016-02-25 12:03:03 +0100

    Directory checking

--- create_vnstat.sh
+++ create_vnstat.sh
@@ -2,19 +2,24 @@
 
 LANG=en_US 
 
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
+
+arg1="${1:-}"
+
+if [ ! -d "$images" ]; then 
+  echo "The directory '${arg1}/images' does not exist."
+  exit 1
+fi
+
 echo "---
 title: vnstat
 taxonomy:
     category: docs
 ---
 "
-# Set magic variables for current file & dir
-__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename ${__file} .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" 
-
-arg1="${1:-}"
 
 vnstati -d -o "${arg1}/images/day.png"
 echo "![vnstat](/images/day.png)"
