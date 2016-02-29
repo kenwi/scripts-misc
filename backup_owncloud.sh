0a481c2e51806ef45c777d16448206d10f13e641
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2016-02-29 13:09:43 +0100

    Rewrite.

--- backup_owncloud.sh
+++ backup_owncloud.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-# Create directory backup and mysql backup of owncloud
-# Script should be symlinked or copied to /var/www
+# Create directory and mysql owncloud backup
 
 date=$(date +"%Y%m%d")
-owncloud="owncloud-dir_"$date
-mysql="owncloud-sql_"$date
 
 read -sp "Enter MySQL password: " mysql_password
 
-if [[ ! -e $owncloud ]]; then
-	mkdir $owncloud
-	cd owncloud
-	
-	rsync -Aaxv . ../$owncloud/
-	cd ..
-fi
+backup_src="/var/www/owncloud"
+backup_dst="/var/backups"
+backup_name="owncloud-backup_${date}.tar"
 
-cd $owncloud
-mysqldump --lock-tables -h localhost -u root -p$mysql_password mysql > $mysql.bak
-tar -v -czf ../$owncloud.tar.gz .
-cd ..
-rm -rf $owncloud
+# Create uncompressed tar archive
+echo "Creating backup: ${backup_dst}/${backup_name}"
+tar cvf "${backup_dst}/${backup_name}" "${backup_src}"
+
+# Dump the db to same directory as owncloud
+echo "Dumping database: ${backup_src}/mysqldump_owncloud_${date}.sql"
+mysqldump --lock-tables -h localhost -u root "-p${mysql_password}" mysql > "${backup_src}/mysqldump_owncloud_${date}.sql"
+
+# Update archive with db dump
+echo "Updating archive with database dump"
+tar uvf "${backup_dst}/${backup_name}" "${backup_src}/mysqldump_owncloud_${date}.sql"
+
+# And remove the dump
+echo "Removing database dump"
+rm "${backup_src}/mysqldump_owncloud_${date}.sql"
