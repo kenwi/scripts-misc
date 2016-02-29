6fb661c0f68b82991dc36bfa0a492ec9dc284fe7
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2016-02-29 13:29:15 +0100

    Letsencrypt backup.

---
+++ backup_letsencrypt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create directory and mysql owncloud backup
+
+date=$(date +"%Y%m%d")
+backup_src="/etc/letsencrypt"
+backup_dst="/var/backups"
+backup_name="letsencrypt-backup_${date}.tar"
+
+# Create uncompressed tar archive
+echo "Creating backup: ${backup_dst}/${backup_name}"
+tar cvf "${backup_dst}/${backup_name}" "${backup_src}"
