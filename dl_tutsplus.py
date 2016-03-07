2c3dd76fd7bb4570219ebb0d133f894cba2d5637
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2016-03-07 22:37:39 +0100

    Does now save and name the files in the directories.

--- dl_tutsplus.py
+++ dl_tutsplus.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
+import os
 from time import sleep
 import sys
 import urllib
@@ -17,7 +18,7 @@ from pyvirtualdisplay import Display
 class TutsPlusDigger:
     def __init__(self, url, username, password):
         print 'Initing hidden display'
-        self.display = Display(visible=0, size=(800,600))
+        self.display = Display(visible=0, size=(1024,768))
         self.downloads = []
         self.url = url
         self.username = username
@@ -26,7 +27,10 @@ class TutsPlusDigger:
     def __enter__(self):
         print 'Starting hidden display and webdriver'
         self.display.start()
-        self.driver = webdriver.Firefox()
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("media.volume_scale", "0.0")
+        
+        self.driver = webdriver.Firefox(firefox_profile=profile)
         return self
                 
     def __exit__(self, type, value, tb):
@@ -38,7 +42,14 @@ class TutsPlusDigger:
         self.login()
         print 'Visiting %s' % self.url
         self.driver.get(self.url)
-        self.get_page_downloads()
+        while True:            
+            self.get_page_downloads()
+            try:
+                self.driver.get(self.url)
+                next = self.driver.find_element_by_xpath("//a[contains(@class, 'next-button')]")
+                next.click()
+            except NoSuchElementException:
+                break
         
     def login(self):
         self.driver.get("https://tutsplus.com/sign_in?redirect_to=https%3A%2F%2Ftutsplus.com%2F")
@@ -67,6 +78,11 @@ class TutsPlusDigger:
     def get_downloads_video_urls(self):
         for download in self.downloads:
             print 'Finding videos for course "%s"' % download['name']
+            if os.path.exists(download['name']):
+                print "Course exists. Skipping"
+                continue
+            os.mkdir(download['name'])
+            
             self.driver.get(download['url'])
             sleep(1)
             for element in self.driver.find_elements_by_class_name("lesson-index__lesson-link"):
@@ -83,6 +99,16 @@ class TutsPlusDigger:
             for video in download['videos']:
                 video['mp4'] = self.get_video_mp4(video['url'])
                 
+            i = 0
+            for video in download['videos']:
+                print "Downloading %s" % video['name']
+                save_file = "%02d - %s.mp4" % (i, video['name'])
+                full_path = os.path.join(download['name'], save_file)
+                
+                if not os.path.exists(full_path):
+                    urllib.urlretrieve(video['mp4'], full_path)
+                    i+=1
+                
     def get_video_mp4(self, video_url):
         print 'Finding video mp4 url for %s' % video_url
         self.driver.get(video_url)
@@ -92,6 +118,6 @@ class TutsPlusDigger:
         print mp4_url
         return mp4_url  
         
-address = "https://code.tutsplus.com/courses" if not len(sys.argv) >= 2 else sys.argv[i]
+address = "https://code.tutsplus.com/categories/c/courses" if not len(sys.argv) >= 2 else sys.argv[1]
 with TutsPlusDigger(address, "", "") as digger:
     digger.dig()
