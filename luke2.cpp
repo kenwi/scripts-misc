39e53d5bb7773a89ec5e09ba72c7ae7a9eaed2de
kenwi <kenneth@wilhelmsen.nu>
2016-12-03 02:29:49 +0100

    Create luke2.cpp

---
+++ luke2.cpp
@@ -0,0 +1,29 @@
+/*
+https://julekalender.knowit.no/luker/qTYR3HY7HjTPmoLgq
+
+Output:
++ 1.48561e+09
+4.7e+07iterations run in 1657650893ns total, average : 35.2692ns.
+
+Code:
+*/
+#include <iostream>
+#include <chrono>
+#include <cmath>
+int main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+
+    double i, sum, fib, run;
+    for (run = 0; run < 1000000; run++)
+    {
+        for (i = sum = fib = 0; fib < 1500000000; sum += ((int)fib & 1) ? fib : 0, (fib = floor(pow((1 + sqrt(5)) / 2, i++) / sqrt(5) + 0.5)));
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+    std::cout << "+ " << sum << std::endl;
+    std::cout << (i*run) << " iterations run in " << duration << "ns total, average : " << duration / (i*run) << "ns." << std::endl;
+
+    return 0;
+}
