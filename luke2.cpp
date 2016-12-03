9eb0076d6fbf32f3b0caaac5d21f9b45d29ae2c0
kenwi <kenneth@wilhelmsen.nu>
2016-12-03 02:44:39 +0100

    Update luke2.cpp

--- luke2.cpp
+++ luke2.cpp
@@ -15,9 +15,9 @@ int main()
     auto begin = std::chrono::high_resolution_clock::now();
 
     double i, sum, fib, run;
-    for (run = 0; run < 1000000; run++)
+    for (run = 0; run < 1e6; run++)
     {
-        for (i = sum = fib = 0; fib < 1500000000; sum += ((int)fib & 1) ? fib : 0, (fib = floor(pow((1 + sqrt(5)) / 2, i++) / sqrt(5) + 0.5)));
+        for (i = sum = fib = 0; fib < 13e8; sum += ((int)fib & 1) ? fib : 0, (fib = floor(pow((1 + sqrt(5)) / 2, i++) / sqrt(5) + 0.5)));
     }
 
     auto end = std::chrono::high_resolution_clock::now();
