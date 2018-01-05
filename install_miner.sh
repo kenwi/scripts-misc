286bfd9b5f6063b7decc56bf27201b48e85fa0f8
kenwi <kenneth@wilhelmsen.nu>
2018-01-05 17:07:27 +0100

    Update install_miner.sh

--- install_miner.sh
+++ install_miner.sh
@@ -5,3 +5,4 @@ sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-de
 cd veriumMiner
 ./build.sh
 echo ./cpuminer -n 1048576 -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u WfqGj6Cp2lW8tabDCT_2.WorkerName -p WorkerPassword > run.sh
+chmod +x run.sh
