fd4e5a8ff63c00ec83de535c5f8b6640942b7d8f
kenwi <kenneth@wilhelmsen.nu>
2018-01-21 09:55:02 +0100

    Added parameter passthrough.

--- install_miner.sh
+++ install_miner.sh
@@ -4,5 +4,5 @@ git clone https://github.com/fireworm71/veriumMiner
 sudo apt install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev zlib1g-dev -y
 cd veriumMiner
 ./build.sh
-echo ./cpuminer -n 1048576 -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u WfqGj6Cp2lW8tabDCT_2.$(hostname) -p p2lW8tabDCTXf > run.sh
+echo ./cpuminer -n 1048576 -o stratum+tcp://eu.vrm.mining-pool.ovh:3032 -u WfqGj6Cp2lW8tabDCT_2.$(hostname) -p p2lW8tabDCTXf $@ > run.sh
 chmod +x run.sh
