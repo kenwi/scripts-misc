e3589b6a224bf4313e547718596b074bd9e5926e
kenwi <kenneth@wilhelmsen.nu>
2018-09-25 04:07:44 +0200

    Update fine_structure_constant.cpp

--- fine_structure_constant.cpp
+++ fine_structure_constant.cpp
@@ -1,7 +1,7 @@
 /*
 Implementation is probably wrong and clearly outputs the incorrect value for the constant Ч.
 
-OUTPUT: 
+OUTPUT:
 1 0
 2 0.25
 3 0.33300532016668060730779643563437
@@ -22,7 +22,7 @@ Euler Gamma: 0.57721566490153286554942724251305
 Inverse alpha: 0.16025980299670170015069459168444
 Fine structure constant (1/alpha): 6.2398678976323269651516056910623
 
-https://drive.google.com/file/d/1WPsVhtBQmdgQl25_evlGQ1mmTQE0Ww4a/view 
+https://drive.google.com/file/d/1WPsVhtBQmdgQl25_evlGQ1mmTQE0Ww4a/view
 
 */
 
@@ -31,32 +31,36 @@ https://drive.google.com/file/d/1WPsVhtBQmdgQl25_evlGQ1mmTQE0Ww4a/view
 
 int main() {
 	std::cout.precision(32);
-	const auto che = ([](const int iterations) {
+	const auto che = ([](const int iterations, void (output)(const double, const double sum) ) {
 		double sum = 0, previous = 0;
+		
 		for (double j = 1; j <= iterations; j++) {
-			std::cout << j << " " << sum / 2 << std::endl;
-			
 			const double integral = ((j + 1 / j)  * log(j) - j + 1 / j) / log(2);
 			sum += pow(2, -j) * (1 - integral);
-			
+
+			output(j, sum);
 			if (sum == previous)
 				break;
 			previous = sum;
 		}
-		return sum / 2; 
+		return sum / 2;
+	});
+
+	const auto output = ([](const double j, const double sum) {
+		std::cout << j << " " << sum / 2 << std::endl;
 	});
 
 	double π = 3.1415926535897932384626433832795,
 		γ = 0.5772156649015328606065120900824024310421,
-		Ч = che(100),
+		Ч = che(100, output),
 		α = Ч * π / γ,
 		Ж = 1 / α;
 
-	std::cout << std::endl;
-	std::cout << "Che: "  << Ч << std::endl;
-	std::cout << "Euler Gamma: " << γ << std::endl;
-	std::cout << "Inverse alpha: " << α << std::endl;
-	std::cout << "Fine structure constant (1/alpha): " << Ж << std::endl;
+	std::cout << std::endl
+		<< "Che: " << Ч << std::endl
+		<< "Euler Gamma: " << γ << std::endl
+		<< "Inverse alpha: " << α << std::endl
+		<< "Fine structure constant (1/alpha): " << Ж << std::endl;
 
 	return 0;
 }
