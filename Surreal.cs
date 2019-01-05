b3029033500b3b82f48ddfc1077a0964bba494ff
kenwi <3333105+kenwi@users.noreply.github.com>
2019-01-05 08:07:57 +0100

    Update Surreal.cs

--- Surreal.cs
+++ Surreal.cs
@@ -58,23 +58,39 @@ namespace MatheMagics
         static void Main(string[] args)
         {
             var Ø = new List<SurrealNumber>();
+
             var zero = new SurrealNumber(Ø, Ø);
-            var zero2 = new SurrealNumber(Ø, Ø);
-
-            Debug.Assert(zero > zero2 == false);
-            Debug.Assert(zero < zero2 == true);
-
             var one = new SurrealNumber(new List<SurrealNumber> { zero }, Ø);
+            var two = new SurrealNumber(new List<SurrealNumber> { one }, Ø);
+            var three = new SurrealNumber(new List<SurrealNumber> { two }, Ø);
+            var four = new SurrealNumber(new List<SurrealNumber> { three }, Ø);
+            var five = new SurrealNumber(new List<SurrealNumber> { four }, Ø);
+            var six = new SurrealNumber(new List<SurrealNumber> { five}, Ø);
+            var seven = new SurrealNumber(new List<SurrealNumber> { six }, Ø);
+            var eight = new SurrealNumber(new List<SurrealNumber> { seven }, Ø);
+            var nine = new SurrealNumber(new List<SurrealNumber> { eight }, Ø);
+
+            var minusZero = new SurrealNumber(Ø, Ø);
+            var minusOne = new SurrealNumber(Ø, new List<SurrealNumber> { zero });            
+            var minusTwo = new SurrealNumber(Ø, new List<SurrealNumber> { minusOne });            
+            var minusThree = new SurrealNumber(Ø, new List<SurrealNumber> { minusTwo });
+            var minusFour = new SurrealNumber(Ø, new List<SurrealNumber> { minusThree });
+            var minusFive = new SurrealNumber(Ø, new List<SurrealNumber> { minusFour });
+            var minusSix = new SurrealNumber(Ø, new List<SurrealNumber> { minusFive });
+            var minusSeven = new SurrealNumber(Ø, new List<SurrealNumber> { minusSix });
+            var minusEight = new SurrealNumber(Ø, new List<SurrealNumber> { minusSeven });
+            var minusNine = new SurrealNumber(Ø, new List<SurrealNumber> { minusEight });
+
+            Debug.Assert(zero > minusZero == false);
+            Debug.Assert(zero < minusZero == true);
             Debug.Assert(one > zero == true);
             Debug.Assert(one < zero == false);
-
-            var minusOne = new SurrealNumber(Ø, new List<SurrealNumber> { zero });
             Debug.Assert(minusOne > one == false);
             Debug.Assert(zero < minusOne == false);
-
-            var minusTwo = new SurrealNumber(Ø, new List<SurrealNumber> { minusOne });
             Debug.Assert(minusTwo < one == true);
             Debug.Assert(minusTwo > one == false);
+            Debug.Assert(nine > minusNine == true);
+            Debug.Assert(minusEight > eight == false);
         }
     }
 }
