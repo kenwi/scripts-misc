8ee56f680f624cc30d391dfe5e1c9afa3649974a
kenwi <3333105+kenwi@users.noreply.github.com>
2019-01-13 22:05:09 +0100

    Update SimdAdd.cs

--- SimdAdd.cs
+++ SimdAdd.cs
@@ -1,3 +1,30 @@
+/*
+
+Testing SIMD for .NET core
+
+Output:
+[0] Elapsed = 00:00:00.0588736 SIMD
+[1] Elapsed = 00:00:00.1233100 NO SIMD
+[0] Elapsed = 00:00:00.0611137 SIMD
+[1] Elapsed = 00:00:00.1282144 NO SIMD
+[0] Elapsed = 00:00:00.0640393 SIMD
+[1] Elapsed = 00:00:00.1328272 NO SIMD
+[0] Elapsed = 00:00:00.0598064 SIMD
+[1] Elapsed = 00:00:00.1213708 NO SIMD
+[0] Elapsed = 00:00:00.0621704 SIMD
+[1] Elapsed = 00:00:00.1270899 NO SIMD
+[0] Elapsed = 00:00:00.1129701 SIMD
+[1] Elapsed = 00:00:00.1769870 NO SIMD
+[0] Elapsed = 00:00:00.0589078 SIMD
+[1] Elapsed = 00:00:00.1218389 NO SIMD
+[0] Elapsed = 00:00:00.0593559 SIMD
+[1] Elapsed = 00:00:00.1214641 NO SIMD
+[0] Elapsed = 00:00:00.0621073 SIMD
+[1] Elapsed = 00:00:00.1272637 NO SIMD
+[0] Elapsed = 00:00:00.0615560 SIMD
+[1] Elapsed = 00:00:00.1275641 NO SIMD
+
+*/
 using System;
 using System.Numerics;
 using System.Runtime.CompilerServices;
